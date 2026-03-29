3b3bb3ef86d8dea3d0e4ef8be769f39b77b94eb3
Ritika Dhanda <21bcs8217@cuchd.in>
2026-03-29 16:41:30 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
+
   name: {
     type: String,
     required: true
@@ -21,6 +22,7 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+
 });
 
 module.exports = mongoose.model("User", UserSchema);
