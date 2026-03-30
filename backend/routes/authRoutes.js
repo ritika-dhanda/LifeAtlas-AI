dc4c44e45e65fc4f76a66329845e5d23ae472218
Ritika Dhanda <21bcs8217@cuchd.in>
2026-03-30 15:44:56 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const authController = require("../controllers/authController");
+const { register, login } = require("../controllers/authController");
 
-// Register
-router.post("/register", authController.register);
-
-// Login
-router.post("/login", authController.login);
+router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
